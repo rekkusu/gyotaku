c14c1724b676c64e403e37ea3bbfd4c1c88fe46e
Rex <rex@rex.gs>
2018-09-18 13:51:40 +0900

    extended timeout

--- app/curl.go
+++ app/curl.go
@@ -8,15 +8,14 @@ import (
 )
 
 func GetWebPage(url string) string {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	output := make(chan string)
 	errch := make(chan error)
 	var cmd *exec.Cmd
 	go func() {
-		//cmd = exec.CommandContext(ctx, "curl", "-s", "-S", "-L", "-c", "/dev/null", "-m", "1", url)
-		cmd = exec.CommandContext(ctx, "curl", "-s", "-S", "-c", "/dev/null", "-m", "2", url)
+		cmd = exec.CommandContext(ctx, "curl", "-s", "-S", "-c", "/dev/null", "-m", "5", url)
 		stdout, _ := cmd.CombinedOutput()
 		output <- string(stdout)
 	}()
